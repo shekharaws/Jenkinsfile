29524a8b929cfa1d83c5e39ce07765f781577078
Shekhar <shekharg1497@gmail.com>
2022-09-27 16:26:14 +0530

    kk

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,7 +1,7 @@
 def checkout_git(String repo) {
     
     echo "This is for checking out git code."
-   git credentialsId: 'githubaccess', url: "git@github.com:shekharaws/${repo}.git"    
+   git credentialsId: 'githubaccess', url: "git@github.com:shekharaws/${repo}.git" , branch: "${branch}"   
 
 }
 
