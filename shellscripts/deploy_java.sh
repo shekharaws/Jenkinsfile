acab50678755100e0ebe13777a7c82f01b2bb073
shekher <shekharg1497@gmail.com>
2022-11-12 12:37:37 +0530

    changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -2,9 +2,9 @@
 mkdir /var/lib/jenkins/artifacts
 cd /var/lib/jenkins/artifacts
 aws s3 cp s3://my-bucket-ss/java-code//sparkjava-hello-world-1.0.war .
-scp sparkjava-hello-world-1.0.war ec2-user@10.0.0.44:/home/ec2-user/tomcat/apache-tomcat-9.0.64/webapps
+scp sparkjava-hello-world-1.0.war ec2-user@10.0.0.44:/home/ec2-user/tomcat/apache-tomcat-9.0.68/webapps
 ssh ec2-user@10.0.0.44 << HERE
-    cd /home/ec2-user/tomcat/apache-tomcat-9.0.64/bin/
+    cd /home/ec2-user/tomcat/apache-tomcat-9.0.68/bin/
     ./shutdown.sh
     ./startup.sh
 HERE    
