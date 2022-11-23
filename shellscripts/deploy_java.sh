a748bcdde784f1819e6e11128f9e535327443797
shekher <shekharg1497@gmail.com>
2022-11-23 12:10:18 +0530

    s

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -6,7 +6,7 @@ IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 
 mkdir /var/lib/jenkins/artifacts
 cd /var/lib/jenkins/artifacts
-aws s3 cp s3://my-bucket-ss/java-code/$tag/hello-world-war-1.0.0.war .
+aws s3 cp s3://my-bucket-ss/java-folder-2/$tag/hello-world-war-1.0.0.war .
 scp hello-world-war-1.0.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.68/webapps
 ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.68/work
