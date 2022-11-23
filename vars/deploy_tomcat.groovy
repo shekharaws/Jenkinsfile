90ba53fb415120a7438037feb620c82fc578fc3f
shekher <shekharg1497@gmail.com>
2022-11-23 12:16:46 +0530

    kk

--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -1,6 +1,6 @@
 def deploy_tomcat( String tag, String env) {
 
-    echo "deploying to tomcat server."
+     echo "deploying to tomcat server."
     sh """
         cd ${WORKSPACE}/shellscripts
         sh ./deploy_java.sh ${tag} ${env}
