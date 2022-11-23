1d93b66b0c8cdbffcc39b18ad150611afd6b9e0d
shekher <shekharg1497@gmail.com>
2022-11-23 12:02:30 +0530

    changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -6,9 +6,9 @@ IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 
 mkdir /var/lib/jenkins/artifacts
 cd /var/lib/jenkins/artifacts
-aws s3 cp s3://my-bucket-ss/java-code//sparkjava-hello-world-1.0.war .
-scp sparkjava-hello-world-1.0.war ec2-user@10.0.0.44:/home/ec2-user/tomcat/apache-tomcat-9.0.68/webapps
-ssh ec2-user@10.0.0.44 << HERE
+aws s3 cp s3://my-bucket-ss/java-code/$tag/sparkjava-hello-world-1.0.war .
+scp sparkjava-hello-world-1.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.68/webapps
+ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.68/work
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.68/bin/
     ./shutdown.sh
